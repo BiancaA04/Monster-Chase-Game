0a839c64307da0064fa3264aa03ca1766c9ccf59
Bianca Aquino <lyahbianca.aquino04@gmail.com>
2022-09-30 23:06:32 +0800

    Added main menu scene and navigations.

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision){
-        if (collision.CompareTag("Enemy") !! collision.CompareTag("Player")){
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Enemy") || collision.CompareTag("Player"))
+        {
             Destroy(collision.gameObject);
         }
     }
